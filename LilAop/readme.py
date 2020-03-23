3fb995d8f0f8c0a5b82b0226f40425a4b2eac87b
looksocii <aopup1122@gmail.com>
2020-03-23 21:16:00 +0700

    aop

--- LilAop/readme.py
+++
@@ -1,9 +0,0 @@
-"""
-    Author
-    -----------------------------------
-    Puntakarn Kaewsanghan 61070139
-    -----------------------------------
-    - pip install django
-    - pip install mysqlclient
-    - pip install mysql-connector
-"""
